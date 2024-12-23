7bbc624d949846361679488b34cc650b6ff3354b
Pritam Jirage <163889796+pj0452003@users.noreply.github.com>
2024-12-23 19:28:06 +0530

    Commit changes in submodule

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\FARHAN\real-time-fake-news-detection\backend\venv
+set VIRTUAL_ENV=C:\Users\prita\Downloads\real-time-fake-news-detection\real-time-fake-news-detection\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
